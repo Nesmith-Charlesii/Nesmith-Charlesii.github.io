48a3a095ffa22b94eb424f4bd2219ba5c40c371a
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-19 18:03:52 -0400

    initial commit. index.js setup complete

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = (props) => {
+    return (
+        <div className="">
+            <h1>Initial Commit</h1>
+        </div>
+    )
+}
+
+export default App;
