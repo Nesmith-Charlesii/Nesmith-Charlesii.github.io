f72fe77f871074caf951f5c5685576fd31bf86cf
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-19 20:30:25 -0400

    created skeleton of navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link} from 'react-router-dom';
 
 const App = (props) => {
     return (
-        <div className="">
-            <h1>Initial Commit</h1>
+        <div className="container-fluid">
+            <nav className="navbar">
+                <div className="brand-name">Brand Name</div>
+                <div className="navbar-links">
+                    <ul>
+                        <li><Link to="bio">Bio</Link></li>
+                        <li><Link to="tech">Tech/Stacks</Link></li>
+                        <li><Link to="projects">Projects</Link></li>
+                        <li><Link to="contact">Contact</Link></li>
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
