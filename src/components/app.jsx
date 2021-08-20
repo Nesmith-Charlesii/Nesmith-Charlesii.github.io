8c897c84cba932e1fda8e87dc057fbc7dfc2800f
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-20 19:22:10 -0400

    positioned profile image with absolute property. Navbar positioning complete

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
+import {Link, Switch, Route} from 'react-router-dom';
 
 const App = (props) => {
+
+    
     return (
         <div className="container-fluid">
             <nav className="navbar d-flex justify-content-center">
@@ -11,11 +13,23 @@ const App = (props) => {
                     <ul>
                         <li><Link to="bio">Bio</Link></li>
                         <li><Link to="tech">Tech/Stacks</Link></li>
+                        <li></li>
+                        <li id="profile-image-container">
+                            <p className="full-name">Charles Nesmith II</p>
+                            <hr/>
+                                <div id="profile-image"></div>
+                            <hr/>
+                            <p className="full-name">Software Developer</p>
+                        </li>
                         <li><Link to="projects">Projects</Link></li>
                         <li><Link to="contact">Contact</Link></li>
                     </ul>
                 </div>
             </nav>
+            <Switch>
+                <Route path="/"> 
+                </Route>
+            </Switch>
         </div>
     )
 }
