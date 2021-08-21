ebe660f91d963df45be4b20ed9902ac904ba005c
Nessiah919 <nesmith.charlesii@gmail.com>
2021-08-20 23:18:19 -0400

    created 'backgroundAnimation' function in App component which created circles in the background of navbar hero onclick of bio button. this is a test and will change in later edition of project

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,13 +5,30 @@ import {Link, Switch, Route} from 'react-router-dom';
 
 const App = (props) => {
 
-    
+    const backgroundAnimation = () => {
+        let backgroundAnimation = document.getElementById("background-animation");
+        let navBar = document.getElementById("navBar");
+        backgroundAnimation.style.height = `40px`;
+        backgroundAnimation.style.width = `40px`;
+        let cloneAnimation;
+        for(let i = 1; i <= 3; i++) {
+            cloneAnimation = backgroundAnimation.cloneNode(true);
+            navBar.insertBefore(cloneAnimation, navBar.childNodes[0]);
+            backgroundAnimation.style.position = "absolute";
+            backgroundAnimation.style.top = `${Math.floor(Math.random() * 500)}px`;
+            backgroundAnimation.style.right = `${Math.floor(Math.random() * 300)}px`
+            backgroundAnimation.style.bottom = `${Math.floor(Math.random() * 500)}px`
+        }
+        console.log("clicked")
+    };
+
     return (
         <div className="container-fluid">
-            <nav className="navbar d-flex justify-content-center">
+            <nav className="navbar d-flex justify-content-center" id="navBar">
+                <div id="background-animation"></div>
                 <div className="navbar-links">
                     <ul>
-                        <li><Link to="bio">Bio</Link></li>
+                        <li><Link to="bio" onClick={() => backgroundAnimation()}>Bio</Link></li>
                         <li><Link to="tech">Tech/Stacks</Link></li>
                         <li></li>
                         <li id="profile-image-container">
